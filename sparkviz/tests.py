1cf906b0d56dd76c56893a54315466ae2a25d83d
Holden Karau <holdenkarau@google.com>
2018-01-07 18:08:25 -0800

    Validate that update works (and make update work)

--- sparkviz/tests.py
+++ sparkviz/tests.py
@@ -27,8 +27,15 @@ class TestSparkViz(SQLTestCase):
   data = [(12, "holden", 2, 3.6, "kk6jkq"), (13, "hello kitty", 2, 3.0, None),
           (None, "bad record", None, None, None)]
 
+  @classmethod
+  def setupClass(cls):
+    from IPython.core.interactiveshell import InteractiveShell
+    cls.display_formatter = InteractiveShell.instance().display_formatter
+    super(TestSparkViz, cls).setUpClass()
+
   def test_basic_display(self):
     """Tests that a basic display runs and doesn't throw any exceptions"""
     df = self.sqlCtx.createDataFrame(self.data, self.schema)
-    df_show = df.show()
+    df_show_obj = df.show()
+    df_show_obj._do_display()
     # TODO: Figure out how to capture the display result
